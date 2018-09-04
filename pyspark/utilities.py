2bfcb6e1eb328dae0ec9546ed789388288708670
Anant Gupta <anantguptadbl@gmail.com>
2018-09-04 15:07:57 +0530

    New function generateRowIndex
    New function generateRowIndex

--- pyspark/utilities.py
+++ pyspark/utilities.py
@@ -10,9 +10,17 @@ def dropFirstRowPartitionFunction(index,iterator):
     else:
         yield iterator
 
+# Function to drop first row
 def dropfirstRow(rddObject):
     return(rddObject.mapPartitionsWithIndex(dropFirstRowPartitionFunction))
-  
+
+# Function to create a row index
+def generateRowIndex(dfObject,indexColName):
+    dfObject=dfObject.withColumn(indexColName,lit(1))
+    curSelect=F.row_number().over(Window.partitionBy(indexColName).orderBy(indexColName))
+    dfObject=dfObject.withColumn(indexColName,curSelect)
+    return(dfObject)
+    
 if __name__=="__main__":
     data=sc.TextFile("file:///someFile.txt")
     data=dropfirstRow(data)
