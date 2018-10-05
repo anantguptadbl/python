6620edaf8d202d6332740dab645af23703b3c9be
Anant Gupta <anantguptadbl@gmail.com>
2018-10-05 22:44:31 +0530

    Update gmm_em.py

--- Algorithms/General/gmm_em.py
+++ Algorithms/General/gmm_em.py
@@ -13,20 +13,42 @@ class GMM():
     def setConfig(self,initialGaussianCenteres):
         self.initialGaussianCenteres=initialGaussianCenteres
         self.config={
-            'means' : [[[np.random.choice(self.data[self.cols[i]]) for i in range(len(self.cols))]] * self.initialGaussianCenteres][0],
+            'means' : [[ (max(self.data[i]) - min(self.data[i]) * y * 1.0000) / self.initialGaussianCenteres  for i in self.cols] for y in range(self.initialGaussianCenteres)],
             'deviations' : [[np.random.randint(10) for j in range(len(self.cols))] for i in range(self.initialGaussianCenteres)],
             'weights' : np.random.dirichlet(np.ones(self.initialGaussianCenteres),size=1)[0]  
         }
+        print("Initial means are {}".format(self.config['means']))
+        
     def prob(self,val, mu, sig, probWeight):
         p = probWeight
         #print("Val = {} and mu ={} and sig ={} and probWeight ={}".format(val,mu,sig,probWeight))
         for i in range(len(self.cols)):
             p *= scipy.stats.norm.pdf(val[i], mu[i], sig[i])
+        #print("the value of p is {}".format(p))
         return p
 
+    def getMaxLabel(self,vals):
+        temp=[]
+        for x in range(self.initialGaussianCenteres):
+            temp.append(self.prob(vals, self.config['means'][x],self.config['deviations'][x],self.config['weights'][x]))
+        return(np.argmax(temp))
+
+    
     def expectationStep(self):
         # At this step we will compute the allocation of gaussian centres based on the current data for each point
-        self.data['label']=self.data[self.cols].apply(lambda row: np.argmax(self.prob(row.values, self.config['means'][x],self.config['deviations'][x],self.config['weights'][x]) for x in range(self.initialGaussianCenteres)) ,axis=1)        
+        #self.data['label']=self.data[self.cols].apply(lambda row: np.argmax(self.prob(row.values, self.config['means'][x],self.config['deviations'][x],self.config['weights'][x]) for x in range(self.initialGaussianCenteres)) ,axis=1)        
+        self.data['label']=self.data[self.cols].apply(lambda row: self.getMaxLabel(row.values),axis=1)        
+        # For testing purposes
+        #for y in range(100):
+        #    temp=[]
+        #    for x in range(self.initialGaussianCenteres):
+        #        temp.append(self.prob(self.data[self.cols].loc[y].values, self.config['means'][x],self.config['deviations'][x],self.config['weights'][x]))
+        #    #print("The value of temp is {}".format(temp))
+        #    if(np.argmax(temp)>0):
+        #        print("The value of argmax is {}".format(np.argmax(temp)))
+            
+        
+        print("Unique Labels are {}".format(self.data['label'].unique()))
         #for curRow in range(self.data.shape[0]):
         #    probList=[self.prob(self.data[self.cols].ix[curRow].values, self.config['means'][x],self.config['deviations'][x],self.config['weights'][x]) for x in range(self.initialGaussianCenteres)]
         #    self.data['label'][curRow]=np.argmax(probList)
@@ -36,21 +58,26 @@ class GMM():
         self.config['weights']=[
             (self.data[self.data['label']==x].shape[0]*1.0000) / self.data.shape[0] for x in range(self.initialGaussianCenteres)
         ]
-        self.config['means']=[
-            self.data[self.data['label']==x].mean() for x in range(self.initialGaussianCenteres)
-        ]
+        print("New means are {}".format(
+        [
+            [self.data[self.data['label']==x][y].values.mean() for y in self.cols] for x in range(self.initialGaussianCenteres)
+        ]))
         
         self.config['means']=[
-            self.data[self.data['label']==x][self.cols].mean() for x in range(self.initialGaussianCenteres)
+            [self.data[self.data['label']==x][y].values.mean() for y in self.cols] for x in range(self.initialGaussianCenteres)
         ]
+        print("New Deviations is {}".format([
+            [self.data[self.data['label']==x][y].values.std() for y in self.cols] for x in range(self.initialGaussianCenteres)
+        ]))
         self.config['deviations']=[
-            self.data[self.data['label']==x][self.cols].std() for x in range(self.initialGaussianCenteres)
+            [self.data[self.data['label']==x][y].values.std() for y in self.cols] for x in range(self.initialGaussianCenteres)
         ]
         
         # we will calculate the change in means and return that value
-        print(self.config['means'])
+        print("Means is {}".format(self.config['means']))
         print(oldMean)
-        return(sum([np.linalg.norm(self.config['means'][x]-oldMean[x]) for x in range(self.initialGaussianCenteres)]))
+        return(1)
+        #return(sum([np.linalg.norm(self.config['means'][x]-oldMean[x]) for x in range(self.initialGaussianCenteres)]))
     
     def iterate(self):
         change=999
@@ -58,11 +85,15 @@ class GMM():
         while change > 0.0001:
             self.expectationStep()
             change=self.maximizationStep()
-            print(change)
-            print("Iteration {}".format(iterCounter))
+            #print(change)
+            #print("Iteration {}".format(iterCounter))
             iterCounter=iterCounter+1
 
 # Creating random data
+mu1=[10,10]
+sig1=[[10,0],[0,10]]
+mu2=[20,20]
+sig2=[[10,0],[0,10]]
 x1, y1 = np.random.multivariate_normal(mu1, sig1, 100).T
 x2, y2 = np.random.multivariate_normal(mu2, sig2, 100).T
 xs = np.concatenate((x1, x2))
