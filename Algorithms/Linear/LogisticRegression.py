6a3e6075e21daa2d468c48db96303d999c7fa2bf
Anant Gupta <anantguptadbl@gmail.com>
2018-12-12 14:37:42 +0000

    Update LogisticRegression.py

--- Algorithms/Linear/LogisticRegression.py
+++ Algorithms/Linear/LogisticRegression.py
@@ -1,20 +1,26 @@
-# Initialization
+# Logistic Regression Class
 
 class LogisticRegression():
-    def __init__(self,data):
-        self.weights=np.random.rand(3).reshape(3,1)
+    def __init__(self,X,y):
+        self.X=X
+        self.y=y
+        self.weights=np.random.rand(self.X.shape[1]).reshape(self.X.shape[1],1)
         self.epochs=100
-        self.data=data
 
     def getSigmoid(self,x):
         return 1.000 / (1 + np.exp(-x) )
 
     def runEpoch_GradientDescent(self):
-        prediction=self.getSigmoid(np.matmul(self.data[:,0:3],self.weights))
-        loss=prediction - self.data[:,3].reshape(100,1)
-        gradient=np.matmul(self.data[:,0:3].T,loss)
-        self.weights-=gradient
-        print("The current loss is {}".format(loss))
+        for curEpoch in range(self.epochs):
+            prediction=self.getSigmoid(np.matmul(self.X,self.weights))
+            loss=prediction - self.y.reshape(self.y.shape[0],1)
+            gradient=np.matmul(self.X.T,loss)
+            self.weights-=gradient
+            if curEpoch % 10 ==0:
+                print("For epoch {} the  log loss is {}".format(curEpoch,-np.array(loss).reshape(self.X.shape[0],).sum()))
 
-lr=LogisticRegression(data)
-lr.runEpoch_GradientDescent()
+if __name__=="__main__":
+    X=np.array([[1,2,3],[1,1,0],[4,5,6],[4,5,2]])
+    y=np.array([0,1,0,1])
+    lr=LogisticRegression(X,y)
+    lr.runEpoch_GradientDescent()
