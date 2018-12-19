f08b42cf785ad2e9203b6433ad3683487723528f
Anant Gupta <anantguptadbl@gmail.com>
2018-12-19 15:11:23 +0000

    Update simpleANTLRExample.py

--- NLP/ANTLR/simpleANTLRExample.py
+++ NLP/ANTLR/simpleANTLRExample.py
@@ -13,3 +13,20 @@ import sys
 lexer = psqlLexer("select col1 from table1")
 stream = CommonTokenStream(lexer)
 parser = psqlParser(stream)
+tree=parser.operation()
+listenerVal=psqlListener()
+
+class psqlPrintListener(psqlListener):
+    def enterColumnNames(self,ctx):
+        columnNameContext=ctx.columnName()
+        
+        
+    def enterColumnName(self, ctx):
+        print("ColumnName is %s" % ctx.NONSPACECONTINUOUS())
+        
+    def enterOperation(self, ctx):
+        print(ctx)
+        
+printer=psqlPrintListener()
+walker=antlr4.ParseTreeWalker()
+walker.walk(printer, tree)
