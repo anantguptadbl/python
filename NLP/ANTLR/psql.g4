f5cbdf343ddab24e36911538487ef83bc040508e
Anant Gupta <anantguptadbl@gmail.com>
2018-12-20 01:52:14 +0000

    Add files via upload

--- NLP/ANTLR/psql.g4
+++ NLP/ANTLR/psql.g4
@@ -1,8 +1,9 @@
 grammar psql;
-expression : SELECT (WS)* colNameList;
+expression : SELECT (WS)* colNameList (WS)* FROM (WS)* tableName (WS)*;
 SELECT : 'select' | 'SELECT' | 'Select';
 FROM : 'FROM' | 'from' | 'From';
 colNameList : (NONSPACECONTINUOUS (WS)* COMMA)* (WS)* NONSPACECONTINUOUS;
+tableName : NONSPACECONTINUOUS;
 NONSPACECONTINUOUS : ('a'..'z'|'A'..'Z'|'0'..'9')+;
 WS : ' ' -> skip;
 COMMA : ',';
--- NLP/ANTLR/psql.py
+++ NLP/ANTLR/psql.py
@@ -1,3 +1,6 @@
+# Running the command for generating the lexer and parser
+# antlr4 -Dlanguage=Python2 psql.g4
+
 import pandas as pd
 import numpy as np
 
@@ -16,11 +19,14 @@ class psqlPrintListener(psqlListener):
                 columnNames.append(curElement.symbol.text)
         #print("ColumnName is %s" % ctx.NONSPACECONTINUOUS())
         print("The column names are {}".format(columnNames))
-        
+
+    def enterTableName(self, ctx):
+        print("The table name is {}".format(ctx.children[0].symbol.text))
+
     def enterExpression(self, ctx):
         print(ctx)
 
-inputStream=antlr4.InputStream('select col1,col2')
+inputStream=antlr4.InputStream('select col1,col2 from table1')
 lexer = psqlLexer(inputStream)
 stream = CommonTokenStream(lexer)
 parser = psqlParser(stream)
@@ -29,3 +35,7 @@ listenerVal=psqlListener()
 printer=psqlPrintListener()
 walker=antlr4.ParseTreeWalker()
 walker.walk(printer, tree)
+
+# Some actual data
+data=pd.DataFrame([[1,2,3],[4,5,6],[7,8,9]],columns=['col1','col2','col3'])
+
