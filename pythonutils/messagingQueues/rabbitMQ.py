89d235e2907b8d1843cadcafbe44cc20b361401c
Anant Gupta <anantguptadbl@gmail.com>
2019-01-22 12:08:14 +0000

    Update rabbitMQ.py

--- pythonutils/messagingQueues/rabbitMQ.py
+++ pythonutils/messagingQueues/rabbitMQ.py
@@ -1,1 +1,37 @@
 # This file will provide a hello world for rabbitMQ services
+# RABBIT MQ
+# sudo apt-get install rabbitmq-server 
+# pip install pika
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
+
+########################################
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+
+channel.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
