507b3ba94eea2c34a21a264692af09336f742fe3
Anant Gupta <anantguptadbl@gmail.com>
2019-05-14 19:14:22 +0530

    Update HierarchialAttentionNetwork.py

--- pytorch/rnn/HierarchialAttentionNetwork.py
+++ pytorch/rnn/HierarchialAttentionNetwork.py
@@ -125,3 +125,27 @@ for epoch in range(epochRange):
     if(epoch % 1==0):
         print("For epoch {}, the loss is {}".format(epoch,lossVal))
 print("Autoencoder Training completed")
+
+##### ACTUAL DATA STUFF  ######
+##### CAN BE IGNORED #####
+
+#data=pd.read_csv('hotel-review/train.csv')
+#import nltk
+#validPosTags=['JJ','JJR','JJS','NN','RB','RBR','RBS','VB','VBD','VBG','VBZ']
+def removeUnncessaryTags(x):
+    # For ech sentnece in a row
+    # For each word in a sentence
+    validSentences=[]
+    for curSentence in x.split('.'):
+        validWords=[]
+        for curWord in curSentence.split(' '):
+            if(len(curWord.strip()) > 2):
+                if(nltk.pos_tag([curWord.lower()])[0][1] in validPosTags):
+                    validWords.append(curWord.lower())
+        validSentences.append(validWords)
+    return(validSentences)
+            
+                
+#data=data[0:10000]
+#data['data']=data['Description'].map(lambda x : removeUnncessaryTags(x))
+#data.head(5)
