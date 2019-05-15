d15ecc286463f8bf141456350497d4b0b0b47252
Anant Gupta <anantguptadbl@gmail.com>
2019-05-15 17:42:58 +0530

    Update HierarchialAttentionNetwork.py

--- pytorch/rnn/HierarchialAttentionNetwork.py
+++ pytorch/rnn/HierarchialAttentionNetwork.py
@@ -109,7 +109,7 @@ X=np.random.rand(numRows * numSentences * numWords,inputDim)
 X=X.reshape(totalBatches,numSentences,numWords,inputDim)
 
 # Creating Y
-Y=np.random.randint(2,size=(numRows))
+Y=np.random.randint(2,size=(numRows,1))
 
 for epoch in range(epochRange):
     lossVal=0
@@ -118,34 +118,10 @@ for epoch in range(epochRange):
         dataInput=torch.autograd.Variable(torch.Tensor(X[curBatch]))
         dataOutput=model(dataInput)
         #print(dataOutput.size())
-        loss=lossCalc(dataOutput,dataInput.view(step_size*batch_size1,-1))
+        loss=lossCalc(dataOutput,torch.Tensor(Y[curBatch]))
         loss.backward()
         lossVal = lossVal + loss
         optimizer.step()
     if(epoch % 1==0):
         print("For epoch {}, the loss is {}".format(epoch,lossVal))
 print("Autoencoder Training completed")
-
-##### ACTUAL DATA STUFF  ######
-##### CAN BE IGNORED #####
-
-#data=pd.read_csv('hotel-review/train.csv')
-#import nltk
-#validPosTags=['JJ','JJR','JJS','NN','RB','RBR','RBS','VB','VBD','VBG','VBZ']
-def removeUnncessaryTags(x):
-    # For ech sentnece in a row
-    # For each word in a sentence
-    validSentences=[]
-    for curSentence in x.split('.'):
-        validWords=[]
-        for curWord in curSentence.split(' '):
-            if(len(curWord.strip()) > 2):
-                if(nltk.pos_tag([curWord.lower()])[0][1] in validPosTags):
-                    validWords.append(curWord.lower())
-        validSentences.append(validWords)
-    return(validSentences)
-            
-                
-#data=data[0:10000]
-#data['data']=data['Description'].map(lambda x : removeUnncessaryTags(x))
-#data.head(5)
