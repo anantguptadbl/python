0bd369e6081570b94ee7250b51d2c9683f0950a0
Anant Gupta <anantguptadbl@gmail.com>
2019-07-15 18:38:38 +0530

    Update simpleLSTM.py

--- pytorch/rnn/simpleLSTM.py
+++ pytorch/rnn/simpleLSTM.py
@@ -1,41 +1,52 @@
+####################################
+# CORRELATION WITH LS
+####################################
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+
 class LSTMSimple(nn.Module):
-    def __init__(self,inputDim,hiddenDim):
+    def __init__(self,inputDim,hiddenDim,batchSize,outputDim):
         super(LSTMSimple,self).__init__()
         torch.manual_seed(1)
-        self.lstm=nn.LSTM(inputDim,hiddenDim)
+        self.lstm=nn.LSTM(inputDim,hiddenDim,1)
         # Hidden state is a tuple of two states, so we will have to initialize two tuples
-        self.hidden = (torch.randn(1,1,3) , torch.rand(1,1,3))
+        self.state_h = torch.randn(1,batchSize,hiddenDim)
+        self.state_c = torch.rand(1,batchSize,hiddenDim)
+        self.linearModel=nn.Linear(hiddenDim,outputDim)
         
     def forward(self,inputs):
-        self.fullDataOutput=[]
-        for i in inputs:
-            self.out,self.hidden = lstm(i.view(1,1,-1),self.hidden)
-            self.fullDataOutput.append(self.out)
-        return self.fullDataOutput
+        # LSTM
+        output, self.hidden = self.lstm(inputs, (self.state_h,self.state_c) )
+        self.state_h=self.state_h.detach()
+        self.state_c=self.state_c.detach()
+        # LINEAR MODEL
+        output=self.linearModel(output)
+        return output
 
 def lossCalc(x,y):
     return torch.sum(torch.add(x,-y))
-    #logsoftmax = nn.LogSoftmax()
-    #return torch.mean(torch.sum(- pred  * logsoftmax(soft_targets), 1))
     
 # Model Object
-model=LSTMSimple(3,3)
+batchSize=5
+inputDim=10
+outputDim=1
+stepSize=24
+hiddenDim=3
+model=LSTMSimple(inputDim,hiddenDim,batchSize,outputDim)
 loss = torch.nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # Input Data
-dataInput = [torch.randn(1, 3) for _ in range(5)]  # make a sequence of length 5
-dataY = [torch.randn(1, 3) for _ in range(5)]  # make a sequence of length 5
-for epoch in range(1000):
-    dataOutput=model(dataInput)
-    loss=lossCalc(torch.stack(dataOutput).view(5,3),torch.stack(dataY).view(5,3))
+dataInput = torch.randn(stepSize,batchSize,inputDim) 
+dataY = torch.randn(stepSize,batchSize,outputDim) 
+for epoch in range(10000):
     optimizer.zero_grad()
-    loss.backward(retain_graph=True)
+    dataOutput=model(dataInput)
+    curLoss=loss(dataOutput.view(batchSize*stepSize,outputDim),dataY.view(batchSize*stepSize,outputDim))
+    curLoss.backward()
     optimizer.step()
-    if(epoch % 100==0):
-        print("For epoch {}, the loss is {}".format(epoch,loss))
+    if(epoch % 1000==0):
+        print("For epoch {}, the loss is {}".format(epoch,curLoss))
