70f8f8f4ec4c77ce5c92ce1a8af66309ca634a43
Anant Gupta <anantguptadbl@gmail.com>
2020-01-20 18:51:36 +0530

    Update quantizingPreTrainedModels.py

--- imageProcessing/quantizingPreTrainedModels.py
+++ imageProcessing/quantizingPreTrainedModels.py
@@ -9,6 +9,7 @@ import torch.utils.data as data
 import torchvision
 from torchvision import transforms
 import torchvision.models as models
+import pickle
 
 # VGG Model
 vgg16 = models.vgg16(pretrained=True)
@@ -44,8 +45,8 @@ class LimitDataset(data.Dataset):
         return self.dataset[i]
 
 
-trainset=LimitDataset(torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform_train, download=False),1500)
-testset=LimitDataset(torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform_test, download=False),1500)
+trainset=LimitDataset(torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform_train, download=True),100)
+testset=LimitDataset(torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform_test, download=True),100)
 #trainset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform_train, download=False)
 #testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test) 
 print("Cell Execution Completed")   
@@ -65,61 +66,64 @@ class Transfer(nn.Module):
         ( \
         vgg16.features, \
         Flatten(), \
-        nn.Linear(512*7*7,200), \
-        nn.Linear(200,64), \
-        nn.Linear(64,32), \
-        nn.Linear(32,10) \
+        nn.Linear(512*7*7,10) \
         ) 
         self.smax=nn.Softmax(dim=1)
         
     def forward(self,x):
-        #x=self.vgg16(x)
-        #x=self.layer1(x.view(-1,512*7*7))
-        #x=self.layer1(x.reshape(-1,512*7*7))
-        #x=self.layer2(x)
-        #x=self.layer3(x)
-        #x=self.layer4(x)
         x=self.model1(x)
         x=self.smax(x)
         return(x)
     
-model=Transfer().cuda()
+model=Transfer()
 optimizer = optim.SGD(model.parameters(), lr=0.01,momentum=0.6)
-criterion = nn.CrossEntropyLoss().cuda()
+criterion = nn.CrossEntropyLoss()
 
 # Config
-epochs=10
-batchSize=16
+epochs=2
+batchSize=8
 
 # Data Loader
 import torch.utils.data as data
-train_data_loader = data.DataLoader(trainset, batch_size=batchSize, shuffle=False,  num_workers=1)
-test_data_loader  = data.DataLoader(testset, batch_size=batchSize, shuffle=False, num_workers=1) 
+train_data_loader = data.DataLoader(trainset, batch_size=batchSize, shuffle=False,  num_workers=0)
+test_data_loader  = data.DataLoader(testset, batch_size=batchSize, shuffle=False, num_workers=0) 
 
 # Model Training
-model=Transfer().cuda()
+model=Transfer()
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
-for epoch in range(10000):     
+for epoch in range(epochs):     
     totalLoss=0
     for step, (x, y) in enumerate(train_data_loader):
         if(step <= 100):
             model.zero_grad()
-            output = model(x.cuda())      
-            loss = criterion(output, y.cuda())   
+            output = model(x)      
+            loss = criterion(output, y)   
             totalLoss=totalLoss + loss.item()         
             loss.backward()                 
             optimizer.step()
     if(epoch%1==0):
         print("Epoch {0} Step {1} TotalLoss {2}".format(epoch,step,totalLoss))
         
-print("Cell Execution Completed")
-
 model=model.to('cpu')
 torch.save(model,"torchNonQuantizedModel")
 print(os.path.getsize("torchNonQuantizedModel")/1e6)  # 79.003
 
+print("Cell Execution Completed")
+
+# POST TRAINING STATIC QUANTIZATION
+# Load the non quantized model
+model=torch.load("torchNonQuantizedModel")
+
+# Fuse Model
+model.eval()
+
+# Eval Model
+#model.fuse_model()
+
+# Setting up the configuration
 model.qconfig = torch.quantization.default_qconfig
 print(model.qconfig)
+# Model Preparation : This will inject the observers
 torch.quantization.prepare(model, inplace=True)
 
 for curModule in model._modules['model1']._modules['0']:
@@ -131,6 +135,15 @@ for curModule in model._modules['model1']._modules['0']:
     if(type(curModule)==torch.nn.modules.activation.ReLU):
         print(curModule.inplace)
         
+# Calibration of the model
+print("Calibrating the model")
+num_calibration_batches=10
+# Evaluation where we are passing data to the model after the observers have been injected through prepare function
+with torch.no_grad():
+    for step, (x, y) in enumerate(train_data_loader):
+        yHat = model(x)
+        loss = criterion(yHat, y)
+
 # Quantization of the model
 newModel=torch.quantization.convert(model,inplace=False)
 torch.save(newModel.state_dict(), "torchQuantizedModel")
@@ -149,4 +162,6 @@ for step, (x, y) in enumerate(train_data_loader):
         output=model.smax(dequantized)
         loss = criterion(output, y)   
         totalLoss=totalLoss + loss.item()         
-print(totalLoss)
+        print(totalLoss)
+        
+print("Cell Execution Completed")
