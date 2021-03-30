a22df9d5e0123f3b9035d167639fdb8a7224bb8c
Anant Gupta <anantguptadbl@gmail.com>
2021-03-30 10:46:22 +0530

    Update inject_docstring.py

--- pythonutils/inject_docstring.py
+++ pythonutils/inject_docstring.py
@@ -1,3 +1,7 @@
+####################################
+# Simple Injection on a string
+####################################
+
 class MyTransformer(ast.NodeTransformer):
     def visit_FunctionDef(self, node):
         doc_string_found = 0
@@ -33,3 +37,40 @@ print("New String of the function")
 print(astunparse.Unparser(modified, sys.stdout))
 exec(compile(modified, '<string>', 'exec'))
 print(add.__doc__)
+
+
+####################################
+# Simple Injection on a file
+####################################
+
+class MyTransformer(ast.NodeTransformer):
+    def visit_FunctionDef(self, node):
+        doc_string_found = 0
+        for cur_element in node.body:
+            #print("Line No and col Offset")
+            #print(cur_element.lineno)
+            #print(cur_element.col_offset)
+            if isinstance(cur_element, ast.Expr) == True:
+                if isinstance(cur_element.value, ast.Str) == True:
+                    print(cur_element.value.s)
+                    doc_string_found = 1
+        # Now we will have to inject the doc string
+        s = ast.Str("Injected a new docstring")
+        new_expr = ast.Expr(value=s)
+        if doc_string_found == 0:
+            node.body.insert(0, new_expr)
+        return node
+
+expr = ast.parse(open("sample_code_wthout_class.py").read())
+expr_ast = ast.parse(expr)
+print(ast.dump(expr_ast))
+unmodified = ast.parse(expr)
+exec(compile(unmodified, '<string>', 'exec'))
+transformer = MyTransformer()
+modified = transformer.visit(unmodified)
+ast.fix_missing_locations(modified)
+print("New String of the function")
+print(astunparse.Unparser(modified, sys.stdout))
+
+with open("sample_code_modified.py", "w") as f:
+    f.write(astunparse.unparse(modified))
